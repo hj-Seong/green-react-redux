55ec9ac30bac90fa6ab6ebb9c367c2d9a725bbd5
hj-Seong <hjseong1222@gmail.com>
2022-11-01 15:48:11 +0900

    1101 리덕스 기본2

--- redux-app/src/components/CounterBox.jsx
+++ redux-app/src/components/CounterBox.jsx
@@ -1,13 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
+import { increase } from "../modules/counter";
 
 const CounterBox = () => {
+    // useSelector를 통해서 state의 원하는 값을 가져올수 있다
     const number = useSelector((state)=>(state.number))
+    // useDispatch를 통해서 사용할 함수를 가져옴
     const dispatch = useDispatch();
     return (  
         <div>
             <h1>카운트입니다</h1>
             <h3>{number}</h3>
-            <button onClick={()=>{ dispatch({type: "increase"}) }} >증가</button>
+
+            <button onClick={()=>{ 
+                // dipatch를 통해 {type: "액션"}을 전달해서 사용
+                // >>> counter의 리듀서 함수로 가서 같은 타입을 찾은 후 실행
+                // >>> 객체값을 직접입력할 경우 오타 및 실수가 있을수 있어
+                //     값을 변경하지 않고 사용하기 위해 counter에서 가져와서 사용
+                dispatch( increase() ) }} >
+                증가
+            </button>
+            {/** counter의 리듀서 함수를 수정해서 1씩 감소하는 버튼작성 */}
+            <button onClick={()=>{dispatch({type:"decrease"})}}>감소</button>
         </div>
     );
 }
--- redux-app/src/modules/counter.js
+++ redux-app/src/modules/counter.js
@@ -4,11 +4,17 @@ const initalState = {
     number : 0,
 }
 
+// dispatch에 들어갈 {type:"액션"} 객체를 함수로 만들어서 내보내줌
+export const increase = () => ({type:"increase"}) // 리턴값 객체
+
+
 // 리듀서 함수
 function counter(state = initalState, action) {
     switch(action.type) {
         case "increase":
             return { ...state , number : state.number+1};
+        case "decrease":
+            return { ...state, number : state.number-1}
         default :
             return state;
     }
