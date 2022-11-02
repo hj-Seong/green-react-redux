4f9e0c6e24cd3269ea1ce73bd43e556ffb0e4eb4
hj-Seong <hjseong1222@gmail.com>
2022-11-02 15:31:09 +0900

    1102 메모추가 콜백확인

--- redux-app/src/components/Memo.jsx
+++ redux-app/src/components/Memo.jsx
@@ -1,6 +1,8 @@
 import { useCallback } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { addmemo } from "../modules/memo";
 
 const Memo = () => {
     const memolist = useSelector((state)=>state.memo.memolist);
@@ -10,14 +12,29 @@ const Memo = () => {
 
     // useDispatch()를 통해서, 메모가 추가한 새로운 리스트를 생성 > memo.js 모듈생성
     const dispatch = useDispatch();
-    const addMemo = useCallback(()=>{}, [])
 
+    // useCallback을 사용해줄때, 입력값이 있다면 입력값에 따라바뀔수 있도록추가
+    // 매개변수로 받아오는 값은 고정되지않음, 매개변수외의 값은 전부 고정
+    const addMemo = useCallback(()=>{ 
+        dispatch( addmemo( {title:title, text:text} ) )},[dispatch,title,text])
+    
+    useEffect(()=>{
+        console.log(title,text)
+    })
     return (  
         <div>
             <input type="text" onChange={(e)=>{setTitle(e.target.value)}}/>
             <input type="text" onChange={(e)=>{setText(e.target.value)}}/>
             <button onClick={addMemo}>메모</button>
-            {memolist[0].title}
+            {   
+                // map: 배열의 있는 요소들 가져와서 return하는 함수
+                memolist.map((memo)=>(
+                    <div>
+                        <h3>{memo.id}. {memo.title}</h3>
+                        <p>{memo.text}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
--- redux-app/src/modules/memo.js
+++ redux-app/src/modules/memo.js
@@ -7,14 +7,29 @@ const initalState = {
             title : "제목",
             text : "내용"
         }
-    ]
+    ],
+    id : 2
 }
+// 액션타입을 함수로 만들어서 내보내줌
+// addmemo에서 받아오는 매개변수 memo는 {title: "", text:""}의 형태로 사용할 예정
+export const addmemo = (memo) => ({type:"addMemo", payload: memo})
+
+
 
 // 리듀서값 작성
 function memo(state=initalState, action) {
     switch(action.type) {
         case "addMemo":
-            return
+            // 새로운 메모생성
+            console.log(action.payload)
+            const newMemo = {
+                id : state.id,
+                title : action.payload.title,
+                text : action.payload.text
+            }
+            // 새로운 메모를 가지는 리스트
+            const newMemoList = state.memolist.concat(newMemo)
+            return { ...state, memolist : newMemoList, id : state.id+1}
         default:
             return state;
     }
