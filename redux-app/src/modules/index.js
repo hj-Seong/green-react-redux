52e4dc8c4aed60e2de9251dda5521e3042279622
hj-Seong <hjseong1222@gmail.com>
2022-11-04 14:48:47 +0900

    1104 리덕스사가

--- redux-app/src/modules/index.js
+++ redux-app/src/modules/index.js
@@ -2,11 +2,20 @@
 import { combineReducers } from "redux";
 
 // 작성한 리덕스 모듈을 가져옴
-import counter from "./counter";
+import counter, { counterSaga } from "./counter";
 import memo from "./memo";
 import news from "./news";
 
+// 리덕스 사가의 내용을 연결해서 사용
+import { all } from "redux-saga/effects";
+
 // 작성한 리덕스를 객체로 묶어서 내보냄
 const rootReducer = combineReducers({counter, memo, news});
+
+// 모든 사가들을 묶어서 내보낼수 있게 rootSaga를 만들어줌
+export function* rootSaga() {
+    yield all([counterSaga()]);
+}
+
 
 export default rootReducer
