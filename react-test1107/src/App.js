1d06e5bc057db0acf90734b83fd3a6c6c919947e
hj-Seong <hjseong1222@gmail.com>
2022-11-08 15:13:12 +0900

    1108 리액트과제

---
+++ react-test1107/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './page/Home';
+import Memo from './page/Memo';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/memo' element={<Memo />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
